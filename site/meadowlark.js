316afb95f3a527adc44936c3c2c08367a392207c
sandilem7789 <sandilem7789@gmail.com>
2021-01-21 03:55:49 +0200

    Handlebars Introduction

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -10,28 +10,21 @@ app.set("view engine", "handlebars");
 
 app.set("port", process.env.port || 3000);
 
-app.get("/", (req, res) => {
-    res.type("text/plain");
-    res.send("Meadowlark Travel");
-});
-app.get("/about", (req, res) => {
-    res.type("text/plain");
-    res.send("About Meadowlark Travel");
-});
+app.get("/", (req, res) => res.render("home"));
+app.get("/about", (req, res) => res.render("about"));
 
 //custom 404 page
 app.use((req, res) => {
     res.type("text/plain");
     res.status(404);
-    res.send("404 - Not found");
+    res.render("404");
 });
 
 //custom 500 page
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.type("text/plain");
+    console.error(err.message);
     res.status(500);
-    res.send("500 - Server Error");
+    res.render("500");
 });
 
 app.listen(app.get("port"), () => {
